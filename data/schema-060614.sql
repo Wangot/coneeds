730b2f4fef26890d04de9eead9e4138e764e8af7
itchad <richard.malibiran@98labs.com>
2014-06-07 04:58:57 +0800

    history table needs

--- data/schema-060614.sql
+++ data/schema-060614.sql
@@ -29,3 +29,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `credits_history` (
+  `reference_code` bigint(20) DEFAULT NULL,
+  `action` varchar(40) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `balance` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
