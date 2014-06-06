f1442893a869f0002ddffc4e6f54db82a4c3caba
erick <erickson.leynes@98labs.com>
2014-06-07 00:04:39 +0800

    modified sql fields

--- data/alter-060614-2.sql
+++ data/alter-060614-2.sql
@@ -2,5 +2,6 @@ ALTER TABLE  `user` ADD  `short_desc` VARCHAR( 255 ) NULL AFTER  `id` ,
 ADD  `keywords` VARCHAR( 255 ) NULL AFTER  `short_desc` ,
 ADD  `screen_name` VARCHAR( 40 ) NULL AFTER  `keywords` ,
 ADD  `lat` DECIMAL( 10, 8 ) NULL AFTER  `screen_name` ,
-ADD  `long` DECIMAL( 10, 8 ) NULL AFTER  `lat` ,
-ADD  `is_professional` TINYINT( 1 ) NULL AFTER  `long`
+ADD  `lng` DECIMAL( 10, 8 ) NULL AFTER  `lat` ,
+ADD  `is_professional` TINYINT( 1 ) NULL AFTER  `lng`,
+ADD  `credits` INT(11) NULL AFTER  `is_professional`;
