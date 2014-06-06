353cce45dcd86d2bfef53697f62c56fd234dd13c
Mardan Tesoro <mardan.tesoro@gmail.com>
2014-06-06 18:53:30 +0800

    Included SQL Dev User Account

--- data/create-database-privileges.sql
+++ data/create-database-privileges.sql
@@ -3,3 +3,9 @@ CREATE DATABASE coneeds;
 CREATE USER coneeds@localhost IDENTIFIED BY 'coneeds';
 
 grant CREATE,INSERT,DELETE,UPDATE,SELECT,DROP,ALTER on coneeds.* to coneeds@localhost;
+
+CREATE DATABASE coneeds_dev;
+
+CREATE USER coneeds_dev@localhost IDENTIFIED BY 'coneeds_dev';
+
+grant CREATE,INSERT,DELETE,UPDATE,SELECT,DROP,ALTER on coneeds_dev.* to coneeds_dev@localhost;
